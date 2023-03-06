6e285589384cd9e98f0fe17d811df4f70020c83e
Aunttwister <pavle.curcic678@gmail.com>
2023-03-06 17:25:32 +0100

    *update for production 1

--- reservations-api/src/Reservations.Api/Program.cs
+++ reservations-api/src/Reservations.Api/Program.cs
@@ -70,15 +70,22 @@ builder.Services.AddAuthorization(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.InjectStylesheet("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui.css");
+        options.InjectJavascript("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui-bundle.js", "text/javascript");
+        options.InjectJavascript("https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.3/swagger-ui-standalone-preset.js", "text/javascript");
+        options.SwaggerEndpoint($"v1/swagger.json", "Reservations API");
+    });
 }
+else
 
-app.UseCustomExceptionHandler();
+    app.UseCustomExceptionHandler();
 
 app.UseStaticFiles();
 
