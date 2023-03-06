12cf417f2ad656f57a8fbb09ef1e5c2adb536627
Aunttwister <pavle.curcic678@gmail.com>
2023-03-06 07:22:25 +0100

    *updated swagger for production purpose

--- reservations-api/src/Reservations.Api/Program.cs
+++ reservations-api/src/Reservations.Api/Program.cs
@@ -70,10 +70,11 @@ builder.Services.AddAuthorization(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
 
