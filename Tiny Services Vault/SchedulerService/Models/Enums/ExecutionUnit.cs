a539be9cc124391c4cd18c55743b045d94d9a980
Aunttwister <pavle.curcic678@gmail.com>
2023-09-14 15:06:38 +0200

    initial push of tiny services vault

---
+++ Tiny Services Vault/SchedulerService/Models/Enums/ExecutionUnit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerService.Models.Enums
+{
+    public enum ExecutionUnit
+    {
+        Year, Month, Week, Day, Hour, Minute
+    }
+
+    public static class ExecutionUnitExtensions
+    {
+        public static ExecutionUnit? StringToExecutionUnit(this string value)
+        {
+            var lowerCase = value.ToLower();
+            switch (lowerCase)
+            {
+                case "minute":
+                    return ExecutionUnit.Minute;
+                case "hour":
+                    return ExecutionUnit.Hour;
+                case "day":
+                    return ExecutionUnit.Day;
+                case "week":
+                    return ExecutionUnit.Week;
+                case "month":
+                    return ExecutionUnit.Month;
+                case "year":
+                    return ExecutionUnit.Year;
+                default:
+                    return null;
+            }
+        }
+    }
+}
